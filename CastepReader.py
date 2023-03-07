129fabab87a8118188078dd5f9fc0c613bef8641
pablogila <pablo.gila.herranz@gmail.com>
2023-03-07 12:07:31 +0100

    worksgit add .

--- CastepReader.py
+++ CastepReader.py
@@ -1,33 +1,18 @@
-""""
-
-This program will read specific data from several .castep files.
-There is a folder called /data, and inside that folder, there are many nested folders, each containing a file named 'cc-2.castep'.
-The nested folders have names like '---pnam-p-1-000-000-270-000---350', and this program extracts the numbers 'xxx-xxx-xxx-xxx' and uses them as reference for the extracted data.
-This program will read the data from the cc-2.castep file in each folder, and output a 'castep.out.csv' file with the data I want, in the root folder.
-I want to write the following columns of data in the .csv file, in this order:
-- name of the parent folder
-- enthalpy of the final iteration, in eV
-- enthalpy of the final iteration, in J
-- cell parameter a, in Angstroms
-- cell parameter b, in Angstroms
-- cell parameter c, in Angstroms
-- cell parameter alpha, in degrees
-- cell parameter beta, in degrees
-- cell parameter gamma, in degrees
-- volume of the cell, in Angstroms^3
-- density of the cell, in amu/Angstroms^3
-- density of the cell, in g/cm^3
-
-"""
+print("\n")
+print("CastepReader  v2023.03.07  by  Pablo Gila-Herranz")
+print("https://github.com/pablogila/CrystalReader ")
+print("\n")
 
 
 import re
 import os
 import csv
 import time
+# Progress bar: for i in tqdm(list): etc
+#from tqdm import tqdm
 
 
-""" This function will extract the numbers from the name of the parent folder. """
+# This function will extract the numbers from the name of the parent folder
 def naming(string):
     # Define a regular expression pattern to match the desired value
     pattern = r"pnam-p-1-(\d{3})-(\d{3})-(\d{3})-(\d{3})"
@@ -42,7 +27,7 @@ def naming(string):
         return None
 
 
-""" This function will extract the value of a given variable from a raw string. """
+# This function will extract the value of a given variable from a raw string
 def extract(string, name):
     pattern = re.compile(name + r'\s*=\s*(-?\d+(?:\.\d+)?(?:[eE][+\-]?\d+)?)')
     match = pattern.search(string)
@@ -52,17 +37,7 @@ def extract(string, name):
         return None
 
 
-""" This function will exclusively extract the density in g/cm^3, because that line in particular is a pain in the ass. """
-def extract_damm_densityg(string):
-    pattern = re.compile(r'=\s*([\d\.]+)')
-    match = pattern.search(string)
-    if match:
-        return float(match.group(1))
-    else:
-        return None
-
-
-""" This function will search for a specific string value in a given file, and return the corresponding line """
+# This function will search for a specific string value in a given file, and return the corresponding line
 def searcher(filename, search_value):
     with open(filename, 'r') as file:
         file.seek(0, 2)  # move the file pointer to the end of the file
@@ -78,51 +53,67 @@ def searcher(filename, search_value):
     return None
 
 
+# This function will print a progress bar in the console, just for fun
+def progressbar(current_value,total_value):
+    bar_length = 40
+    percentage = int((current_value/total_value)*100)
+    progress = int((bar_length * current_value ) / total_value)
+    loadbar = "Progress: [{:{len}}]{}%".format(progress*'■',percentage,len=bar_length)
+    print(loadbar, end='\r')
 
+
+# Start a timer to measure the execution time. Just for fun.
 time_start = time.time()
 
+# For the progress bar
+i = 0
 
-
+# Define the conversion factor from eV to kJ/mol
+# THIS NEEDS TO BE FIXED
 ev = 1.602176634E-19 / 1000 # eV to kJ
 
-row = []
 
 # Get the absolute path to the directory containing the Python script
 dir_path = os.path.dirname(os.path.realpath(__file__))
-# Specify the path to the directory containing the folders relative to the script's directory
-path = os.path.join(dir_path, 'data') # This is the path to the folder containing the folders with the .castep files
-# Get the names of all the directories in the given path
+# Specify the path to the directory containing the folders with the .castep files, relative to the script's directory
+path = os.path.join(dir_path, 'data')
+# Get the names of all the directories in the given path, and store them in a list
 directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
-print(" ")
 print("Starting to read files...")
 print(" ")
 
-# Here we open the file to write the data
+# Open the output file to write the data
 with open('out_castep.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     # Write a header row for the CSV file
-    writer.writerow(['file', 'enthalpy', 'enthalpy*ev', 'a', 'b', 'c', 'alpha', 'beta', 'gamma', 'cell', 'density', 'density_g'])
+    writer.writerow(['filename', 'enthalpy / eV', 'enthalpy / kJ/mol', 'a', 'b', 'c', 'alpha', 'beta', 'gamma', 'cell volume / A^3', 'density / amu/A^3', 'density / g/cm^3'])
 
-    # Let's loop through all the folders in the path
+    # Loop through all the folders in the /data path
     for directory in directories:
 
-        file = os.path.join(path, directory, 'cc-2.castep')
+        # Progress bar, just for fun
+        i+=1
+        progressbar(i,len(directories))
 
+        # Define the path to the .castep file
+        file = os.path.join(path, directory, 'cc-2.castep')
         file_name = naming(directory)
 
+        # Read the file and look for the desired lines
         enthalpy_str = searcher(file, 'LBFGS: Final Enthalpy     =')
-        cell_str = searcher(file, 'Current cell volume =')
+        volume_str = searcher(file, 'Current cell volume =')
         density_str = searcher(file, 'density =')
-        densityg_str = searcher(file, '=') # This line in particular is weird so be careful
+        densityg_str = searcher(file, '=')
         a_str = searcher(file, 'a =')
         b_str = searcher(file, 'b =')
         c_str = searcher(file, 'c =')
 
+        # Extract the values from the strings
         enthalpy = extract(enthalpy_str, 'LBFGS: Final Enthalpy')
-        cell = extract(cell_str, 'Current cell volume')
+        volume = extract(volume_str, 'Current cell volume')
         density = extract(density_str, 'density')
-        densityg = extract_damm_densityg(densityg_str)
+        densityg = extract(densityg_str, '')
         a = extract(a_str, 'a')
         b = extract(b_str, 'b')
         c = extract(c_str, 'c')
@@ -130,25 +121,31 @@ with open('out_castep.csv', 'w', newline='') as file:
         beta = extract(b_str, 'beta')
         gamma = extract(c_str, 'gamma')
 
-        print(file_name)
-        #print("enthalpy = ",enthalpy)
-        #print("cell = ",cell)
-        #print("density = ",density)
-        #print("densityg = ",densityg)
-        #print("a = ",a)
-        #print("b = ",b)
-        #print("c = ",c)
-        #print("alpha = ",alpha)
-        #print("beta = ",beta)
-        #print("gamma = ",gamma)
-        #print("")
-
-        # create a dataframe with the data
-        row = [file_name, enthalpy, enthalpy*ev, a, b, c, alpha, beta, gamma, cell, density, densityg]
+        # write the data row to the file
+        row = [file_name, enthalpy, enthalpy*ev, a, b, c, alpha, beta, gamma, volume, density, densityg]
         writer.writerow(row)
 
-print("Done!")
+        # Print the data on screen, for debugging purposes
+        #print(file_name)
+        #print("")
+        #print("enthalpy = ", enthalpy)
+        #print("enthalpy*ev = ", enthalpy*ev)
+        #print("a = ", a)
+        #print("b = ", b)
+        #print("c = ", c)
+        #print("alpha = ", alpha)
+        #print("beta = ", beta)
+        #print("gamma = ", gamma)
+        #print("volume = ", volume)
+        #print("density = ", density)
+        #print("densityg = ", densityg)
+        #print("")
 
+
+print("")
+print("Finished reading the '.castep' files\nData extracted and saved to 'out_castep.csv'")
+print("")
 
 time_elapsed = (time.time() - time_start)
 print("Completed in ", time_elapsed, " seconds")
+print("")
--- README.md
+++ README.md
@@ -4,19 +4,37 @@ The purpose of this set of programs is to automate the reading and extracting of
 
 ## CastepReader
 
-The `CastepReader.py` program recursively reads a folder called `data`, which itself contains several folders, each containing a cc-2.castep file.\
-Naming example: `data/pnam-p-1-000-000-180-000---400/cc-2.castep`\
-The program extracts the relevant data from these files, starting from the end of the file, and writes everything in an `out_castep.csv`, in the following columns:
+The `CastepReader.py` program recursively reads a folder called `/data`, which contains several nested folders, each containing a `cc-2.castep` file.\
+Naming example: `data/pnam-p-1-000-000-180-000---400/cc-2.castep`.\
+The program iterates over this set of files, starting to read from the end of the file, and writes the relevant data to an `out_castep.csv', line by line, on each iteration. The columns written contain the following data:
 
 * name of the parent folder, in `xxx-xxx-xxx-xxx` format
-* enthalpy of the final iteration, in eV
-* enthalpy of the final iteration, in kJ/mol (NEEDS FIXING)
-* cell parameter a, in Angstroms
+* final enthalpy, in eV
+* final enthalpy, in kJ/mol -----NEEDS FIXING-----
+* cell parameter a, in $\r{A}$
 * cell parameter b
 * cell parameter c
 * cell parameter alpha, in degrees
 * cell parameter beta
 * cell parameter gamma
-* volume of the cell, in Angstroms^3
-* density of the cell, in amu/Angstroms^3
-* density of the cell, in g/cm^3
+* cell volume, in $\r{A}^3$
+* density of the cell, in amu/$\r{A}^3$
+* density of the cell, in g/cm$^3$
+
+### CastepReader Functions
+
+The program `CastepReader.py` calls the following functions:
+
+* `searcher(filename, search_value)`. This function reads a given file in `filename` from the end, until it finds a line starting with `search_value`, and then returns the entire line as an output string.
+
+* `extract(string, name)`. This function extracts the float value of a given variable from a raw string by searching the given string for a matching pattern as `name + r'\s*=\s*(-?\d+(?:\.\d+)?(?:[eE][+\-]?\d+)?)'`, where:
+  * `\s*=\s*` matches zero or more whitespace characters, followed by an equals sign, followed by zero or more whitespace characters
+  * `-?` matches an optional minus sign
+  * `\d+` matches one or more digits
+  * `(?:\.\d+)?` matches an optional decimal point followed by one or more digits
+  * `(?:[eE][+\-]?\d+)?` matches an optional exponent in scientific notation, which consists of an "e" or "E" character, an optional plus or minus sign, and one or more digits.
+
+&NewLine;
+* `naming(string)`. This function reads the name of the parent folder, and returns its name in the `xxx-xxx-xxx-xxx` format.
+
+* `progressbar()`. Gives you a hint as to whether or not you can go out and get coffee.
